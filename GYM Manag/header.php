2fb69c3b3f6b4ccd2686dc86aa3f436227c7a20a
AbhayKumar110 <93936076+AbhayKumar110@users.noreply.github.com>
2022-08-22 11:46:34 +0530

    Add files via upload

---
+++ GYM Manag/header.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'admin-panel.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title> </title>
+   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+<body>
+
+<header>
+ <nav>
+     <div class="main-wrapper">
+	      <ul>
+	            <li><a href="index.php">home</a></li>
+	       </ul>
+		       <div class="nav-login">
+			       <?php
+				        if (isset($_SESSION['u_id'])) {
+						  echo '<form action="includes/logout.inc.php" method="POST">
+					            <button type="submit" name="submit">logout</button>
+					              </form>';	
+   
+							
+			            } else{
+							
+							echo '<form action="includes/login.inc.php" method="POST">
+                              <input type="text" name="uid" placeholder="username/e-mail">
+                              <input type="password" name="pwd" placeholder="password">
+                               <button type="submit" name="submit">login</button>						
+				                </form>';
+							
+						}
+				   
+				    ?>
+					
+				
+		       </div>
+	 </div>
+ </nav>
+
+</header>
